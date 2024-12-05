01b53ee376f220ea011fc08f7a3f1b2736da1b15
Noam Sokolove <62836489+JSninjawizard@users.noreply.github.com>
2024-12-06 00:09:23 +0200

    logic works

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
 const btn = document.querySelector('.btn')
 const labels = document.querySelectorAll('label')
 const inputs = document.querySelectorAll('input')
+const forms = document.querySelectorAll('.form')
 
-
+console.log(forms);
 btn.addEventListener('click', (e)=> {
-    inputs.forEach('')
+
+
+
+    inputs.forEach((inputEl,i,arr) => {
+        const inputIndex = i+1
+      
+        // if(inputEl.classList.contains('input-username')) {
+        //    if (inputEl.value === "") {
+        //     console.log('empty');
+        //    } else {
+        //     console.log(inputEl.value);
+            
+        //     labels.forEach((labelEl,i,arr) => {
+        //         const labelindex = i+1
+        //         if (inputIndex === labelindex) {
+        //             console.log('true');
+        //             labelEl.style.display ='none'
+        //         } else {
+        //             console.log('not true');
+        //         }
+        //     })
+        //    }
+        // }
+
+        if (inputs[0].value !== "") {
+            forms[0].classList.add('visible')
+            forms[1].classList.remove('visible')
+        }
+            
+            
+            if (inputs[1].value !== "") {
+                forms[1].classList.add('visible')
+                forms[2].classList.remove('visible')
+            }
+
+            if (inputs[2].value !== "") {
+                forms[2].classList.add('visible')
+            }
+
+
+    })
 })

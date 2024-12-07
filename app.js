ba40c9a3bde0654a1e08da27c40cc905c9b834c9
Noam Sokolove <62836489+JSninjawizard@users.noreply.github.com>
2024-12-07 09:23:50 +0200

    logic works - on click goes to next label + progress bar works

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 const btn = document.querySelector('.btn')
 const boxes = document.querySelectorAll('.box')
-
+const forms = document.querySelectorAll('.form')
+const inputs = document.querySelectorAll('input')
+// console.log(inputs);
 
 let current = 1
 btn.addEventListener('click', (e) => {
-    console.log(e.target);
+    // console.log(e.target);
     boxes.forEach((el, i, arr) => {
-        if (i < arr.length) {
-            if (i+1 === current) {
-                el.classList.add('pr')
-                console.log(el);
-                console.log(i);
-            }
-        }
+
+        forms.forEach((form, formI, formArr ) => {
+            inputs.forEach((input, inputI, inputArr) => {
+                
+                if (i+1 === current && 
+                    formI +1 === current && 
+                    inputI +1 === current && 
+                    input.value !== "") {
+                        
+                        el.classList.add('pr')
+                        form.classList.add('v0')
+                        
+                        if (form.classList.contains('v0')){
+                            form.nextElementSibling.classList.remove('v0')
+                        }
+                        // console.log(el);
+                        // console.log(i);
+                    }
+
+            })
+
+        })
+        
     })
     current++
 })
+
+const test = [
+    {},
+    {},
+    {}
+]
